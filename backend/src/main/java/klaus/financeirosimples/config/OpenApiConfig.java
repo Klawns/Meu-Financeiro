fa14bd31b3cc230aa89c194a4713481ab967d306
Klawns <fabriciioklaus@gmail.com>
2026-07-27 12:27:42 -0300

    feat: adiciona as info para o swagger

--- backend/src/main/java/klaus/financeirosimples/config/OpenApiConfig.java
+++ backend/src/main/java/klaus/financeirosimples/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package klaus.financeirosimples.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .info(new Info().title("Meu Financeiro").version("1.0").description("Meu Financeiro API Documentation"))
                 .components(new Components()
                         .addSecuritySchemes(
                                 "access-token",
