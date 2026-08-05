d5cf1f4bcbe71c382579b2b373f1a3854c45ec20
Klawns <fabriciioklaus@gmail.com>
2026-08-05 11:48:46 -0300

    feat: adiciona alguns logs simples para os casos de uso do modulo de usuários.

--- backend/src/main/java/klaus/financeirosimples/user/application/usecases/FindUserUseCase.java
+++ backend/src/main/java/klaus/financeirosimples/user/application/usecases/FindUserUseCase.java
@@ -5,18 +5,28 @@ import klaus.financeirosimples.user.application.output.UserOutput;
 import klaus.financeirosimples.user.application.ports.UserRepository;
 import klaus.financeirosimples.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FindUserUseCase {
+
     private final UserRepository userRepository;
 
     public UserOutput findById(UUID id) {
         User user = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
-        return new UserOutput(user.getUsername(), user.getEmail());
+                .orElseThrow(() -> {
+                    log.warn("User not found. userId={}", id);
+                    return new UserNotFoundException("User not found.");
+                });
+
+        return new UserOutput(
+                user.getUsername(),
+                user.getEmail()
+        );
     }
 }
