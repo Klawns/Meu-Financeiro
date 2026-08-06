ff41f730ce5d47cbaefd823c359c0d4bb9784ced
Klawns <fabriciioklaus@gmail.com>
2026-08-06 17:44:06 -0300

    fix: corrige testes do CreateTransactionUseCase

--- backend/src/test/java/klaus/financeirosimples/transactions/usecase/CreateTransactionUseCaseTest.java
+++ backend/src/test/java/klaus/financeirosimples/transactions/usecase/CreateTransactionUseCaseTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -43,25 +44,19 @@ public class CreateTransactionUseCaseTest {
     @Test
     @DisplayName("Success - Should create transaction with success")
     void createTransactionSuccess() {
-        UUID userId = UUID.randomUUID();
 
+        UUID userId = UUID.randomUUID();
         User user = createUser();
 
         when(currentUser.id()).thenReturn(userId);
-
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
 
         CreateTransactionCommand command = createTransactionCommand();
 
-        UUID response = create.execute(command);
+        create.execute(command);
 
-        assertNotNull(response);
-
-        verify(userRepository)
-                .findById(userId);
-
-        verify(transactionRepository)
-                .save(any(Transaction.class));
+        verify(userRepository).findById(userId);
+        verify(transactionRepository).save(any(Transaction.class));
     }
 
     @Test
