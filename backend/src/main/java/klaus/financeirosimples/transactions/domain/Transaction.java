b977d32a47ceef409115d9464b5fe4f10f904fb3
Klawns <fabriciioklaus@gmail.com>
2026-08-06 11:56:21 -0300

    feat: adiciona validações e regra de negócio para o dominio de Transaction

--- backend/src/main/java/klaus/financeirosimples/transactions/domain/Transaction.java
+++ backend/src/main/java/klaus/financeirosimples/transactions/domain/Transaction.java
@@ -1,17 +1,21 @@
 package klaus.financeirosimples.transactions.domain;
 
 import jakarta.persistence.*;
+import klaus.financeirosimples.common.exceptions.DomainException;
 import klaus.financeirosimples.transactions.domain.vo.Money;
 import klaus.financeirosimples.user.domain.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 @Entity
 @Table(name = "transactions")
 @NoArgsConstructor
@@ -47,27 +51,90 @@ public class Transaction {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    public Transaction(User user, TransactionType type, String category, Money amount, String account, LocalDate occurredAt) {
-        this.user = user;
-        this.type = type;
-        this.category = category;
-        this.amount = amount;
-        this.account = account;
-        this.occurredAt = occurredAt;
-    }
-
-    public void update(
+    public Transaction(
+            User user,
             TransactionType type,
             String category,
             Money amount,
             String account,
             LocalDate occurredAt
     ) {
-        this.type = type;
+        this.user = Objects.requireNonNull(user);
+        this.type = Objects.requireNonNull(type);
+        this.category = Objects.requireNonNull(category);
+        this.amount = Objects.requireNonNull(amount);
+        this.account = Objects.requireNonNull(account);
+        this.occurredAt = Objects.requireNonNull(occurredAt);
+
+        validate(
+                this.category,
+                this.amount,
+                this.account,
+                this.occurredAt
+        );
+    }
+
+    public void changeCategory(String category) {
+        Objects.requireNonNull(category);
+        validateCategory(category);
         this.category = category;
+    }
+
+    public void changeAmount(Money amount) {
+        Objects.requireNonNull(amount);
+        validateAmount(amount);
         this.amount = amount;
+    }
+
+    public void changeAccount(String account) {
+        Objects.requireNonNull(account);
+        validateAccount(account);
         this.account = account;
+    }
+
+    public void changeOccurredAt(LocalDate occurredAt) {
+        Objects.requireNonNull(occurredAt);
+        validateOccurredAt(occurredAt);
         this.occurredAt = occurredAt;
     }
 
+    private void validate(
+            String category,
+            Money amount,
+            String account,
+            LocalDate occurredAt
+    ) {
+        validateCategory(category);
+        validateAmount(amount);
+        validateAccount(account);
+        validateOccurredAt(occurredAt);
+    }
+
+    private static void validateCategory(String category) {
+        if (category.isBlank()) {
+            log.warn("Category is required");
+            throw new DomainException("Category is required");
+        }
+    }
+
+    private static void validateAmount(Money amount) {
+        if (amount.amount() <= 0) {
+            log.warn("Transaction amount must be greater than zero");
+            throw new DomainException("Transaction amount must be greater than zero");
+        }
+    }
+
+    private static void validateAccount(String account) {
+        if (account.isBlank()) {
+            log.warn("Account is required");
+            throw new DomainException("Account is required");
+        }
+    }
+
+    private static void validateOccurredAt(LocalDate occurredAt) {
+        if (occurredAt.isAfter(LocalDate.now())) {
+            log.warn("Transaction date cannot be in the future");
+            throw new DomainException("Transaction date cannot be in the future");
+        }
+    }
 }
